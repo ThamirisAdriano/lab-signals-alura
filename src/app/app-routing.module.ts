8d3d995f61bb30c50e25431ffab50a2da94faa35
thamiris <tsadriano.band.com.br>
2024-10-08 06:47:08 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignalsIntroComponent } from './signals-intro/signals-intro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'elements', pathMatch: 'full' }, 
+  { path: 'intro', component: SignalsIntroComponent }, 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignalsIntroComponent } from './signals-intro/signals-intro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignalsIntroComponent
   ],
   imports: [
     BrowserModule,
