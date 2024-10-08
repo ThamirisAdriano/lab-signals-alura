ecdd19d256574fc1cd7b2e70a796bc51ebd82537
thamiris <tsadriano.band.com.br>
2024-10-08 07:35:23 -0300

    aula 1-2

--- src/app/signals-intro/signals-intro.component.ts
+++ src/app/signals-intro/signals-intro.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, signal, computed } from '@angular/core';
+
+interface Elemento {
+  nome: string;
+  simbolo: string;
+  numeroAtomico: number;
+}
 
 @Component({
   selector: 'app-signals-intro',
@@ -6,5 +12,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./signals-intro.component.css']
 })
 export class SignalsIntroComponent {
+  elementoSelecionado = signal<Elemento | null>(null);
 
+  elementos: Elemento[] = [
+    { nome: 'Hidrogênio', simbolo: 'H', numeroAtomico: 1 },
+    { nome: 'Oxigênio', simbolo: 'O', numeroAtomico: 8 },
+    { nome: 'Sódio', simbolo: 'Na', numeroAtomico: 11 },
+    { nome: 'Cloro', simbolo: 'Cl', numeroAtomico: 17 },
+  ];
+
+  selecionarElemento(elemento: Elemento) {
+    this.elementoSelecionado.set(elemento);
+  }
+
+  elementoInfo = computed(() => {
+    const elemento = this.elementoSelecionado();
+    return elemento
+      ? `Nome: ${elemento.nome} (${elemento.simbolo}), Número Atômico: ${elemento.numeroAtomico}`
+      : 'Nenhum elemento selecionado';
+  });
 }
